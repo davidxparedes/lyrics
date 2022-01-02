9f0084607aff479f71cd046e303c54a34fefde7c
David Paredes <dparedes@carpages.ca>
2022-01-02 17:37:11 -0500

    code clean up

--- scripts.js
+++ scripts.js
@@ -6,77 +6,77 @@ app.$recentSearchesToggle = $('#recentSearchesToggle');
 app.lyricsArray = [];
 
 app.getLyrics = (artist, song) => {
-    $.ajax({
-        url: `https://api.lyrics.ovh/v1/${artist}/${song}`,
-        method: "GET",
-        dataType: "json",
-        beforeSend: function () {
-            $('.loader').removeClass('d-none')
-        },
-        complete: function () {
-            $('.loader').addClass('d-none')
-        },
-    }).then(function(data){
-        const lyrics = data.lyrics;
-        app.lyricsArray.push(lyrics);
+  $.ajax({
+    url: `https://api.lyrics.ovh/v1/${artist}/${song}`,
+    method: "GET",
+    dataType: "json",
+    beforeSend: function () {
+        $('.loader').removeClass('d-none')
+    },
+    complete: function () {
+        $('.loader').addClass('d-none')
+    },
+  }).then(function(data){
+    const lyrics = data.lyrics;
+    app.lyricsArray.push(lyrics);
 
-        app.displayData(artist, song, lyrics);
-        app.addRecentSearches(artist, song);
-        app.$recentSearchesToggle.removeClass('d-none').addClass('d-flex');
-    }).catch(function() {
-        $('.lyrics').html('No lyrics available');
-        $('#lyrics-modal').modal('toggle');
-    });
+    app.displayData(artist, song, lyrics);
+    app.addRecentSearches(artist, song);
+    app.$recentSearchesToggle.removeClass('d-none').addClass('d-flex');
+  }).catch(function() {
+    $('.lyrics').html('No lyrics available');
+    $('#lyrics-modal').modal('toggle');
+  });
 };
 
 app.displayData = (artist, song, lyrics) => {
-    const $headingArtist = $('#headingArtist');
-    const $headingSong = $('#headingSong');
-        
-    $headingArtist.html(artist);
-    $headingSong.html(song);
+  const $headingArtist = $('#headingArtist');
+  const $headingSong = $('#headingSong');
+      
+  $headingArtist.html(artist);
+  $headingSong.html(song);
 
-    $('.lyrics').html(lyrics.replace(/\n/g, "<br>"));
-    $('#lyrics-modal').modal('toggle');
+  $('.lyrics').html(lyrics.replace(/\n/g, "<br>"));
+  $('#lyrics-modal').modal('toggle');
 };
 
 app.addRecentSearches = (artist, song) => {
-    $('#recentSearches').append(`
-        <a class="row text-white d-flex justify-content-center text-decoration-none py-2 text-uppercase" href="#">
-            <div class="col-sm-5 d-flex justify-content-center justify-content-sm-end align-items-center text-center text-sm-right p-sm-0">
-                <span class="recent-artist">${artist}</span>
-            </div>
-            <div class="col-sm-1 p-0 d-flex justify-content-center align-items-center">
-                <span class="d-none d-sm-block text-center">-</span>
-            </div>
-            <div class="col-sm-5 d-flex justify-content-center justify-content-sm-start align-items-center text-center text-sm-left p-sm-0">
-                <span class="recent-song">${song}</span>
-            </div>
-        </a>
-    `);
+  $('#recentSearches').append(`
+    <a class="row text-white d-flex justify-content-center text-decoration-none py-2 text-uppercase" href="#">
+      <div class="col-sm-5 d-flex justify-content-center justify-content-sm-end align-items-center p-sm-0">
+        <span class="recent-artist">${artist}</span>
+      </div>
+      <div class="col-sm-1 p-0 d-flex justify-content-center align-items-center">
+        <span class="d-none d-sm-block">-</span>
+      </div>
+      <div class="col-sm-5 d-flex justify-content-center justify-content-sm-start align-items-center p-sm-0">
+        <span class="recent-song">${song}</span>
+      </div>
+    </a>
+  `);
 };
 
 app.init = () => {
-    app.$form.on('submit', function(e) {
-        e.preventDefault();
-    
-        const $artist = $('#artist').val();
-        const $song = $('#song').val();
+  app.$form.on('submit', function(e) {
+    e.preventDefault();
 
-        app.getLyrics($artist, $song);
-        app.$form.trigger('reset');
-    });
+    const $artist = $('#artist').val();
+    const $song = $('#song').val();
 
-    app.$recentSearches.on('click', '.row', function() {
-        const recentItemIndex = $(this).index();
-        const recentArtist = $(this).find('.recent-artist').html();
-        const recentSong = $(this).find('.recent-song').html();
+    app.getLyrics($artist, $song);
+    app.$form.trigger('reset');
+  });
 
-        app.displayData(recentArtist, recentSong, app.lyricsArray[recentItemIndex]);
-        console.log(app.lyricsArray[recentItemIndex]);
-    });
+  app.$recentSearches.on('click', '.row', function() {
+    const recentItemIndex = $(this).index();
+    const recentArtist = $(this).find('.recent-artist').html();
+    const recentSong = $(this).find('.recent-song').html();
+
+    app.displayData(recentArtist, recentSong, app.lyricsArray[recentItemIndex]);
+    console.log(app.lyricsArray[recentItemIndex]);
+  });
 };
 
 $(function() {
-    app.init();
+  app.init();
 });
